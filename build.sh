22b207315ec69cedc834ce07c97714b1435dc815
rocket049 <fuhuizn@163.com>
2020-04-24 10:20:39 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 g++ -c qtermwidget.cpp -fPIC `pkg-config --cflags qtermwidget5`
 g++ -c qtermwidget.moc.cpp -fPIC `pkg-config --cflags qtermwidget5`
 
-go build -ldflags '-s -w'
-#~/go/bin/qtdeploy build
+#go build -ldflags '-s -w'
+~/go/bin/qtdeploy build
 
